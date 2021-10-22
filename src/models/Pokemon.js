7048d65c51ce0c6233fa54674bd44f2a40526c73
Erick Scur <erickscur@gmail.com>
2021-10-22 16:50:38 -0300

    Pokemon model

---
+++ src/models/Pokemon.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const PokemonSchema = new mongoose.Schema({
+    pokedex_index:{
+        type:Number,
+        required: true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    hp:{
+        type:Number,
+        required: true
+    },
+    attack:{
+        type:Number,
+        required:true
+    },
+    defense:{
+        type:Number,
+        required:true
+    },
+    special_attack:{
+        type:Number,
+        required:true
+    },
+    special_defense:{
+        type:Number,
+        required:true
+    },
+    speed:{
+        type:Number,
+        required: true
+    },
+    generation:{
+        type:Number,
+        required:true
+    },
+    Types:{
+        type:Array,
+        required:true
+    }
+});
+mongoose.model('Pokemon', PokemonSchema);
